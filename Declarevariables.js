67f77ae7426325445e5de3544a8c76bf12cd6c93
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-16 12:16:17 +0000

    Declaration_types

---
+++ Declarevariables.js
@@ -0,0 +1,10 @@
+//Various type of declaration in JavaScript
+var myName = "Mohan"; // Use for whole program
+
+let ourName = "Mohanasundaram K"; // Use within the scope
+
+const pi = 3.14 // Can never change when it is declared
+
+console.log(myName);
+console.log(ourName);
+console.log(pi);
