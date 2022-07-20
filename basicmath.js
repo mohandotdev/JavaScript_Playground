f740a24dccdc4076aea5842d1c234c8c86c88e2c
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-20 13:22:48 +0000

    mathoperations

---
+++ basicmath.js
@@ -0,0 +1,16 @@
+//Basic Mathematics in JavaScript
+//Addition
+var sum = 10 + 20;
+console.log(sum);
+//Subtraction
+var sub = 100 - 50;
+console.log(sub)
+//Multiplication
+var mul = 123 * 345;
+console.log(mul);
+//Remainder
+var rem = 15%5;
+console.log(rem);
+//Quotient
+var quo = 10/5;
+console.log(quo);
