f208642982492076fe8931ab08a873dd543a652a
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-20 14:54:16 +0000

    types of escape sequence

--- escapesequence.js
+++ escapesequence.js
@@ -11,8 +11,20 @@ Escape Sequence
 \f form feed
 
 *****/
-var str1 = "\'Billy Butcher\'";
-console.log(str);
-
-var str2 = "\"Home Lander\"";
+var str1 = "\'Billy Butcher\'"; // --> \'single quotes
 console.log(str1);
+
+var str2 = "\"Homelander\""; // --> \" double quotes
+console.log(str2);
+
+console.log("\\"); // --> To print the backslash
+
+console.log("The Boys\nBilly Butcher <--> Homelander"); // --> \n new line
+
+console.log("Hello Javascript\rI am noob"); // \r --> carriage return
+
+console.log("Hello\tWorld:)"); // \t --> tab space
+
+console.log("Hello \bjavascript"); // \b --> backspace
+
+console.log("One Two Three\f Four\f King"); // \f --> form feed
