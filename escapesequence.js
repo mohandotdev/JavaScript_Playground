a6655730cf3a90c999713766de785be036887384
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-20 13:26:25 +0000

    escape sequence

---
+++ escapesequence.js
@@ -0,0 +1,18 @@
+/*****
+Escape Sequence
+
+\' sinle quotes
+\" double quotes
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+
+*****/
+var str1 = "\'Billy Butcher\'";
+console.log(str);
+
+var str2 = "\"Home Lander\"";
+console.log(str1);
