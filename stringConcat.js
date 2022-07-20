c88914e05f174abc234a65d6469d6519336854c8
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-20 15:18:40 +0000

    Concatenating Strings

---
+++ stringConcat.js
@@ -0,0 +1,15 @@
+//Concatenating Strings with plus operator
+var a = "This is first line." + "This is second line";
+console.log(a);
+
+var firstname = "Mohanasundaram";
+var lastname = "Karthikeyan";
+console.log(firstname + " " + lastname);
+
+var b = "I am one.";
+b+= "I am second";
+console.log(b);
+
+var myName = "Mohan";
+var str = "Hello, my name is " + myName + " , have a nice day! :)";
+console.log(str);
