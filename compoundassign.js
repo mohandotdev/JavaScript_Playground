e1c9289ee402e58184559a9349cdde41192a83bf
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-20 13:25:52 +0000

    augumented math operation

---
+++ compoundassign.js
@@ -0,0 +1,20 @@
+//Augumented Math Operations
+//Compound Assignment with Augumented Addition
+var num1 = 100;
+num1 += 250;  //---> (num1 = num1 + 250)
+console.log(num1);
+
+//Compound Assignment with Augumented Subtraction
+var num2 = 173;
+num2 -= 23;  //---> (num2 = num2 - 23)
+console.log(num2);
+
+//Compound Assignment with Augumented Multiplication
+var num3 = 125;
+num3 *= 7;  //---> (num3 = num3 * 7)
+console.log(num3);
+
+//Compound Assignment with Augumented Multiplication
+var num4 = 1678;
+num4 /= 2;
+console.log(num4);
