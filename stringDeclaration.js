fda686e29062a52c75ec84b19ed4ec18b13f1509
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-20 13:26:08 +0000

    strings

---
+++ stringDeclaration.js
@@ -0,0 +1,12 @@
+//String Declaration
+var firstname = "Mohanasundaram";
+var lastname = "Karthikeyan";
+console.log(firstname + " " + lastname);
+
+//Quoting Strings with Single Quotes
+var str = '<a href="https://www.example.com" target="_blank">Link</a>';
+console.log(str);
+
+//Escaping the Literal Quotes in String
+var mystr = "I am a \"double quoted\" string inside \"double quotes\"";
+console.log(mystr);
