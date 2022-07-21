d60e0cb572a51f3324147bfab509393e30f27fb0
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-21 06:09:01 +0000

    Mad lib style word game

---
+++ wordblank.js
@@ -0,0 +1,6 @@
+function wordBlanks(myNoun,myAdjective,myVerb,myAdverb)
+{
+    var result = "The "+ myAdjective +" "+ myNoun +" "+ myVerb +" "+ myAdverb + " to the store";
+    console.log(result);
+}
+console.log(wordBlanks("dog","big","ran","quickly"));
