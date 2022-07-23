ab73109c6a93e4f7395b1f966f5fb43fc34b2634
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-23 03:44:25 +0000

    Function call

---
+++ function.js
@@ -0,0 +1,20 @@
+  //Code Reusable with function
+function ReusableFunction()
+{
+    console.log("Hello JavaScript");
+}
+ReusableFunction();
+ReusableFunction();
+ReusableFunction();
+
+//Passing values to the function
+function addNumber(a,b)
+{
+    console.log(a+b);
+}
+function subNumber(a,b)
+{
+    console.log(a-b);
+}
+addNumber(10,5);
+subNumber(67,12);
