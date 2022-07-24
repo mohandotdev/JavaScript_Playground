cbb5ff045d6982930fe3b0673d348a5b54017222
mohandotdev <rkarthikeyan969@gmail.com>
2022-07-24 18:23:32 +0000

    if else function

---
+++ ifelse.js
@@ -0,0 +1,21 @@
+//if-else statement
+function trueOrfalse(isItTrue)
+{
+    if(isItTrue)
+    {
+        return "Yes, it was true.";
+    }
+    return "No, it was false.";
+}
+console.log(trueOrfalse(true));
+
+//Equality operator
+function testEqual(num)
+{
+    if(num == 100)
+    {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+console.log(testEqual(99));
